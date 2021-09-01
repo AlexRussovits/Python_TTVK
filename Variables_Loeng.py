73e742f9d73ddb76e68077739be3bd770bb0cb19
Aleksander Russovitš <t212552@vk.edu.ee>
2021-09-01 13:57:12 +0300

    Variables

---
+++ Variables_Loeng.py
@@ -0,0 +1,46 @@
+import math
+'''
+#Практика
+numberInt = 10
+numberFloat = 8.4
+text = "Yes"
+numberNotInt = numberInt * 3.5
+numberFloat -= 1
+numberInt / numberFloat
+text = '"' + "No"*2 + "_" + "Yes"*3 + '"'  
+print(numberInt, numberNotInt,numberFloat, text)
+
+print('----------------------------------')
+#Задание 1
+a = int(input('a = '))
+b = int(input('b = '))
+c = int(input('c = '))
+s = a + b + c
+m = a * b * c
+avg = (a+b+c)/3
+print('Результат: ',s)
+print('Результат: ', m) 
+print('Результат: ', round(avg,2))
+#-----------------------------------------------
+print('-----------------------------------------')
+
+#Задание 2
+####1
+a = int(input('a = '))
+b = int(input('b = '))
+summa = a + b
+subt = a - b
+m = a*b
+d = a/b
+q = a**b
+v = a%b
+h = a//b
+
+print('Сумма: ', summa,'Разность: ', subt, 'Произведение: ', m, 'Частное: ',round(d,2), 'Возведение в степень: ', q,
+      'Остаток от деления',round(v,2), 'Целочисленное деление: ', h)
+      '''
+#####2
+side = int(input('Введите сторону квадрата: '))
+P
+
+
