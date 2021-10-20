e84d824a461fa1fbd6b26b34057a363e5f945412
AlexRussovits <sasikrus2002@gmail.com>
2021-10-20 13:16:56 +0300

    functions

---
+++ Functions_Loeng.py
@@ -0,0 +1,101 @@
+import math
+'''
+def doubleWord():
+    a = input("Введите текст или число: ")
+    if a == int:
+      a = int(a)
+      math.pow(a,a)
+      print(a)
+      #return math.pow(a,a)
+    else:
+        a = str(a)
+        a*=len(a)
+        print(a)
+    #return a
+doubleWord()    
+'''
+
+'''
+def doubleWord(a):
+    if type(a) == int:
+        a =int(a)
+        a = math.pow(a,a)
+        #print(a)
+        #return math.pow(a,a)
+    else:
+        a = str(a)
+        a*=len(a)
+        #print(a)
+    return a
+print(doubleWord(8))
+print(doubleWord("xaxa"))
+'''
+
+'''
+def flipValues(a,b):
+    a,b = b,a
+    print(a,b)
+    print ("\n")
+flipValues(8,6)    
+'''
+
+'''
+def WordUpCase(string):
+    string = string.lower()
+    return string.capitalize()
+print(WordUpCase("alex"))
+'''
+
+'''
+def square(a):
+    a = int(input('Input value: '))
+    return math.pow(a,2)
+print("Square: ",square(6))
+'''
+
+'''
+def arithmetic(a,b,operation):
+    
+    if operation == '+':
+         a,operation,b
+    elif operation == '-':
+         a,operation,b
+    elif operation == '*':
+         a,operation,b
+    elif operation == '/':
+         a,operation,b
+    return 'Undefined operation'
+print(arithmetic(5,2,'+'))
+'''
+
+
+
+
+def min
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
